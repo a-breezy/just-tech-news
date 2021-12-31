ec07d29cdeab8bef25825371ef82792c2df263be
a.breezy <ambrose.wilkins@gmail.com>
2021-12-31 10:46:46 -0500

    add post model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // imports User model and exports an object with it as a property
 // (sets up future growth of app)
 const User = require("./User");
+const Post = require("./Post");
 
-module.exports = { User };
+module.exports = { User, Post };
